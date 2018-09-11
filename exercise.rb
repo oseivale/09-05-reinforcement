265d45e07d36d1be0d0c24d194325d17cf5b6720
Valerie Osei <valerie.osei@gmail.com>
2018-09-11 17:47:51 -0400

    practicing parsing api, making api call, and getting info from JSON hash

---
+++ exercise.rb
@@ -0,0 +1,9 @@
+require 'httparty'
+
+toronto_wards_response = HTTParty.get('https://represent.opennorth.ca/boundaries/toronto-wards/')
+
+toronto_wards_json = JSON.parse(toronto_wards_response.body)
+
+toronto_wards_json['objects'].each do |object|
+  p object['name']
+end
