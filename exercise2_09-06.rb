0f1342bac7dffd9701907e1f8ff8644e48d708fe
Valerie Osei <valerie.osei@gmail.com>
2018-09-12 14:55:59 -0400

    practicing API calls

--- exercise2_09-06.rb
+++ exercise2_09-06.rb
@@ -5,3 +5,22 @@ random_password_response = HTTParty.get('https://makemeapassword.org//api/v1/pas
 random_password_json = JSON.parse(random_password_response.body)
 
 p random_password_json['pws']
+
+puts
+
+10.times do |password|
+
+  p random_password_json['pws'][0]
+end
+
+puts
+
+# playing around again with API call to a different URL
+
+# random_password_reponse_2 = HTTParty.get('https://makemeapassword.org/api/v1/readablepassphrase/json?pc=10&s=RandomLong')
+#
+# random_password_json2 = JSON.parse(random_password_reponse_2.body)
+#
+# p random_password_json2['pws']
+#
+# puts
