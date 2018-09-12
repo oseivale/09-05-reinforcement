04fe7e14d91de5690ec34019d3b8a2cc288e2147
Valerie Osei <valerie.osei@gmail.com>
2018-09-12 14:11:32 -0400

    played around with API calls, calling different parts of the API URLs

--- exercise.rb
+++ exercise.rb
@@ -7,3 +7,15 @@ toronto_wards_json = JSON.parse(toronto_wards_response.body)
 toronto_wards_json['objects'].each do |object|
   p object['name']
 end
+
+puts
+
+# Playing around with different API calls
+
+toronto_elections_response = HTTParty.get('https://represent.opennorth.ca/candidates/')
+
+toronto_elections_json = JSON.parse(toronto_elections_response.body)
+
+toronto_elections_json['objects'].each do |object|
+  p object['name']
+end
